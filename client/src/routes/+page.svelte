2e0749d220383807796b941e227b15adf6e0e3e6
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-06-18 00:07:18 +0530

    client: update: home-page: btns

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,27 +1,61 @@
-<div id="buttons"></div>
+<script>
+    import Chessboard from "$lib/Chessboard.svelte";
+</script>
 
-<div id="chessboard" class="inactive"></div>
+<div id="board-container">
+    <Chessboard/>
+</div>
+
+<div id="action-btn-container">
+    <button class="action-btn" id="pub-game-btn">public game</button>
+    <button class="action-btn" id="pvt-game-btn">private game</button>
+</div>
 
 <style>
-    /* #buttons {
-        background: none;
-        border: 2px solid #d3d2d2;
-        position: fixed;
-        margin-left: 50%;
-        margin-top: 25%;
-        transform: translate(-50%, -25%);
-        height: 50px;
-        width: 50%;
-    }
-    
-    #chessboard {
-        background: none;
-        border: 2px solid #d3d2d2;
+    #board-container {
+        height: 60vw;
+        width: 60vw;
         position: fixed;
         margin-left: 50%;
         transform: translateX(-50%);
-        bottom: -900px;
-        height: 70vw; 
-        width: 70%;
-    } */
+        top: 660px;
+        z-index: 2;
+    }
+
+    #action-btn-container {
+        display: inlline;
+        background: none;
+        /* border: 2px solid #d3d2d2; */
+        position: fixed;
+        margin-left: 50%;
+        margin-top: 23%;
+        transform: translate(-50%, -25%);
+        height: 50px;
+        width: 40%;
+    }
+
+    .action-btn {
+        height: 100%;
+        width: 48%;
+        background: #fff;
+        color: #313030;
+        font-family: "Roboto Mono", sans-serif;
+        font-size: 17px;
+        border: 1.5px solid #d3d2d2;
+        cursor: pointer;
+    }
+
+    /* need to find a better hover effect for these buttons */
+    .action-btn:hover {
+        transition: .1s linear;
+        border: 1px solid #acabab;
+    }
+
+    #pvt-game-btn {
+        float: left;
+    }
+
+    #pub-game-btn {
+        float: right;
+    }
 </style>
