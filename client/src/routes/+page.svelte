6ca5128985ed38ef89d52e33976169fb68527c45
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-06-21 00:01:49 +0530

    client: update: main page: game setup

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,10 +1,119 @@
-<script>
+<script lang="ts">
+    // ugly, janky code that works
+    // needs major refactoring
+    // should be ready to accept backend data
+    // move game setup to a separate component
+    // change names for divs and vars, cleanup css
+    // clean up or change the page change mechanism and if expressions
+
+    import { goto } from "$app/navigation";
     import Chessboard from "$lib/Chessboard.svelte";
+
+    enum GameMode {
+        Private,
+        Public
+    }
+
+    enum PrivateGameChoice {
+        None,
+        JoinRoom,
+        CreateRoom
+    }
+
+    enum GameSetupPage {
+        Main,
+        Settings,
+        Loading
+    }
+
+    let selectedMode: GameMode;
+    let selectedPGChoice: PrivateGameChoice = PrivateGameChoice.None;
+    let currentPage: GameSetupPage = GameSetupPage.Main;
+    let lastPage: GameSetupPage;
+    let loadingTimeoutID: number = 0;
+
+    let loadingMessage = "setting up game...";
+
+    function gotoPage(page: GameSetupPage) {
+        lastPage = currentPage;
+        currentPage = page
+    }
+
+    function selectGameMode(mode: GameMode) {
+        selectedMode = mode;
+        if (mode === GameMode.Public) {
+            loadGame();
+        } else {
+            gotoPage(GameSetupPage.Settings)
+        }
+    }
+
+    function selectRoomChoice(choice: PrivateGameChoice) {
+        selectedPGChoice = choice;
+    }
+
+    function cancelChoice() {
+        selectedPGChoice = PrivateGameChoice.None;
+        gotoPage(lastPage)
+    }
+
+    function loadGame() {
+        gotoPage(GameSetupPage.Loading);
+        loadingTimeoutID = setTimeout(() => {goto("/game")}, 3000);
+    }
+
+    function cancelLoad() {
+        if (loadingTimeoutID !== 0) {
+            clearTimeout(loadingTimeoutID);
+            loadingTimeoutID = 0;
+        }
+        gotoPage(lastPage);
+    }
 </script>
 
-<div id="game-btn-container">
-    <a href="/game"><button class="game-btn" id="pub-game-btn">public game</button></a>
-    <button class="game-btn" id="pvt-game-btn">private game</button>
+
+<div id="game-setup-container">
+    {#if currentPage === GameSetupPage.Settings && selectedMode === GameMode.Private && selectedPGChoice === PrivateGameChoice.JoinRoom}
+        <button class="cancel-back-btn" on:click="{cancelChoice}">X</button>
+        <input type="text" placeholder="room code"/>
+        <button on:click={loadGame}>enter room</button>
+    {:else if currentPage === GameSetupPage.Settings && selectedMode === GameMode.Private && selectedPGChoice === PrivateGameChoice.CreateRoom}
+        <button class="cancel-back-btn" on:click="{cancelChoice}">X</button>
+        <button>que-ere-fdq</button>
+        <button>join</button>
+        <button>player has joined!</button>
+        <button on:click={loadGame}>start game</button> 
+    {:else if currentPage === GameSetupPage.Settings && selectedMode === GameMode.Private && selectedPGChoice === PrivateGameChoice.None} 
+        <div id="private-room-choice-container" class="center-container-type-1">
+            <button class="cancel-back-btn" on:click="{cancelChoice}">X</button>
+            <div id="private-room-choice-sub-container">
+                <button id="join-room-btn" class="game-mode-btn choice-btn"
+                    on:click={() => {selectRoomChoice(PrivateGameChoice.JoinRoom)}}>
+                    join room
+                </button>
+                <button id="create-room-btn" class="game-mode-btn choice-btn"
+                    on:click={() => {selectRoomChoice(PrivateGameChoice.CreateRoom)}}>
+                    create game
+                </button>
+            </div>
+        </div>
+    {:else if currentPage === GameSetupPage.Main} 
+        <div id="game-mode-select-container" class="center-container-type-1">
+            <button id="pub-game-btn" class="game-mode-btn choice-btn"
+                on:click="{() => {selectGameMode(GameMode.Public)}}">
+                public game
+            </button>
+            <button id="pvt-game-btn" class="game-mode-btn choice-btn"
+                on:click="{() => {selectGameMode(GameMode.Private)}}">
+                private game
+            </button>
+        </div>
+    {:else} 
+        <div id="game-loading-container" class="center-container-type-1">
+            <button class="cancel-back-btn" on:click="{cancelLoad}">X</button>
+            <button id="game-loading-msg">{loadingMessage}</button> 
+        </div>
+    {/if}
 </div>
 
 <div id="chessboard-container">
@@ -12,26 +121,26 @@
 </div>
 
 <style>
-    #chessboard-container {
-        height: 60vw;
-        width: 60vw;
+    #game-setup-container {
+        height: 175px;
+        width: 50%; /**/
         position: fixed;
+        top: calc(50% - 110px); /**/
         margin-left: 50%;
         transform: translateX(-50%);
-        top: 660px; /**/
+        /* border: 1px solid black;  */
     }
 
-    #game-btn-container {
+    .center-container-type-1 {
         height: 50px;
-        width: 40%; /**/
-        position: fixed;
-        top: calc(50% - 40px); /**/
+        width: 80%;
+        position: relative;
+        top: calc(50% - 25px); /**/
         margin-left: 50%;
         transform: translateX(-50%);
-        display: inline-block;
     }   
 
-    .game-btn {
+    .choice-btn {
         height: 100%;
         width: 48%;
         background: #fff;
@@ -43,15 +152,58 @@
     }
 
     /* find better hover effect for these buttons - maybe?*/
-    .game-btn:hover {
+    .choice-btn:hover {
         border: 2px solid #d3d2d2;
     }
 
-    #pub-game-btn {
+    .choice-btn:nth-child(1) {
         float: left;
     }
 
-    #pvt-game-btn {
+    .choice-btn:nth-child(2) {
         float: right;
+    }
+
+    #private-room-choice-sub-container {
+        height: 100%;
+        width: calc(100% - 70px);
+        float: right;
+    }
+
+    .cancel-back-btn {
+        height: 100%;
+        width: 50px;
+        background: #fff;
+        color: #313030;
+        border: 1.5px solid #d3d2d2;
+        font-size: 17px;
+        font-family: sans-serif;
+        font-weight: bolder;
+        cursor: pointer;
+        float: left;
+    }
+
+    .cancel-back-btn:hover {
+        border: 2px solid #d3d2d2;
+    }
+
+    #game-loading-msg {
+        height: 100%;
+        width: calc(100% - 70px);
+        background: #efefee;
+        color: #A2A2A2;
+        border: none;
+        float: right;
+        font-size: 17px;
+        font-family: "Roboto Mono", sans-serif;
+    }
+
+    #chessboard-container {
+        height: 60vw;
+        width: 60vw;
+        position: fixed;
+        margin-left: 50%;
+        transform: translateX(-50%);
+        top: 660px; /**/
     }
 </style>
