732f43eadaa31c6d452cfe79c9f2003c3d9e86c9
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-06-20 17:27:43 +0530

    client: fix: removed /game from .gitignore

---
+++ client/src/routes/game/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Chessboard from "$lib/Chessboard.svelte";
+</script>
+
+<div id="chessboard-container">
+    <Chessboard/>
+</div>
+
+<style>
+    #chessboard-container {
+        height: 40vw;
+        width: 40vw;
+        position: fixed;
+        top: 90px;
+        margin-left: 50%;
+        transform: translateX(-50%);
+    }
+</style>
