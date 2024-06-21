fb34cb47846f11aa862a32faf7d9da7ce362a803
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-06-22 00:32:35 +0530

    client: update (minor): chessboard transition

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,18 +1,34 @@
 <script>
     import Chessboard from "$lib/Chessboard.svelte";
+    
+    let chessboardClass = "chessboard-down";
+
+    setTimeout(() => {
+        chessboardClass = "chessboard-up";
+    }, 250);
 </script>
 
-<div id="chessboard-container">
+<div id="chessboard-container" class="{chessboardClass}">
     <Chessboard/>
 </div>
 
 <style>
     #chessboard-container {
+        position: fixed;
+        margin-left: 50%;
+        transform: translateX(-50%);    
+        transition: 0.2s linear;
+    }
+
+    .chessboard-down {
+        height: 60vw;
+        width: 60vw;
+        bottom: -45vw;
+    }
+
+    .chessboard-up {
         height: 40vw;
         width: 40vw;
-        position: fixed;
-        top: 90px;
-        margin-left: 50%;
-        transform: translateX(-50%);
+        bottom: 90px;
     }
 </style>
