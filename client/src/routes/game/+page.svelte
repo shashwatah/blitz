ca67bb50356dea3a70d83cba49bf6bf9f8f7b764
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-06-30 18:27:20 +0530

    client: update: game page: rudimentary chat box

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,40 +1,142 @@
 <script lang="ts">
+    import { fade } from "svelte/transition";
+
     import { ChessBoardPos } from "$lib/types/chessBoard";
     import Chessboard from "$lib/components/Chessboard.svelte";
-    
-    let chessboardPos = ChessBoardPos.Pulled;
+
+    let currentChessboardPos = ChessBoardPos.Pulled;
+    let chessboardRendered = false;
+
     setTimeout(() => {
-        chessboardPos = ChessBoardPos.Center;
+        currentChessboardPos = ChessBoardPos.Center;
+        chessboardRendered = true;
     }, 0);
 </script>
 
-<div id="left" class="game-stat"></div>
-<div id="right" class="game-stat"></div>
+{#if chessboardRendered} 
+    <div id="left-aux-container" class="aux-container" transition:fade={{ delay: 400, duration: 100 }}>
+        <div id="chat-box">
+            <div id="chat-box-chats"></div>
+            <input type="text" id="chat-box-input" placeholder="type your message"/>
+        </div>
+        <div id="match-btn-container">
+            <button class="match-btn disabled-btn">abort</button>
+            <button class="match-btn">resign</button>
+            <button class="match-btn">draw</button>
+        </div>
+    </div>
 
-<div id="chessboard-container" class="chessboard-pos-{chessboardPos}">
+    <div id="right-aux-container" class="aux-container" transition:fade={{ delay: 400, duration: 100 }}>
+        <div id="move-board"></div>
+    </div>
+{/if}
+
+<div id="chessboard-container" class="chessboard-pos-{currentChessboardPos}">
     <Chessboard/>
 </div>
 
+
 <style>
-    .game-stat {
-        height: 40vw;
-        width: 25vw;
+    .aux-container {
         position: fixed;
+        height: 30vw;
+        width: 20vw;
         top: 50vh;
         transform: translateY(-50%);
-        border: 1px solid black;
+        /* border: 1px solid black; */
     }
 
-    #left {
-        left: 0px;
+    #left-aux-container {
+        left: 5%;
     }
 
-    #right {
-        right: 0px;
+    #chat-box {
+        position: relative;
+        height: 18vw;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
+        border: 1px solid #d1d1d1;
     }
 
+    #chat-box-chats {
+        position: fixed;
+        top: 0px;
+        height: calc(100% - 44px);
+        width: 100%;
+    }
+
+    #chat-box-input {
+        position: fixed;
+        bottom: .5px;
+        height: 40px;
+        width: calc(100% - 20px);
+        background: #efefef;
+        border: none;
+        border-top: 1px solid #d1d1d1;
+        padding: 0px 10px;
+        font-size: 15px;
+        font-family: "Roboto Mono", sans-serif;
+        color: #313131;
+    }
+
+    #chat-box-input::placeholder {
+        color: #a2a2a2;
+    }
+
+    #match-btn-container {
+        position: fixed;
+        bottom: 0px;
+        /* border: 1px solid green; */
+        height: 40px;
+        width: 100%;
+        text-align: center;
+    }
+
+    .match-btn {
+        height: 100%;
+        width: 30%;
+        /* margin-right: 3.6%; */
+        font-family: "Roboto Mono", sans-serif;
+        font-size: 14px;
+        border: none;
+        color: #a2a2a2;
+        background: #efefef;
+        cursor: pointer;
+    }
+
+    .match-btn:not(.disabled-btn):hover {
+        color: #313131;
+    }
+
+    .match-btn.disabled-btn {
+        color: #d1d1d1;
+    }
+
+    .match-btn:nth-of-type(1) {
+        float: left;
+    }
+
+    .match-btn:nth-of-type(3) {
+        float: right;
+    }
+
+    #right-aux-container {
+        right: 5%;
+    }
+
+    #move-board {
+        position: relative;
+        height: 20vw;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
+        border: 1px solid #d1d1d1;
+        background: #fff;
+    } 
+    
+    
     #chessboard-container {
-        background-color: blue;
         position: fixed;
         margin-left: 50%;
         transform: translateX(-51.5%);  /* ? */
