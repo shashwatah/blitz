f0cc3c79581ded8d57fb9e59e7abe15bc9b2b2ab
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-07-03 22:46:56 +0530

    client: refactor: game

--- client/src/routes/game/+page.svelte
+++ client/src/routes/game/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-    // really janky code, works in the browser though.
-    // worst part: player info 
-    // major refactor needed
+    // refactored but still a bit makeshifty
+    // no idea how media queries are going to work on this
 
     import { fade } from "svelte/transition";
 
     import { ChessBoardPos } from "$lib/types/chessBoard";
     import Chessboard from "$lib/components/Chessboard.svelte";
-
     import { chessPieces } from "$lib/data/chessPieces";
 
     let currentChessboardPos = ChessBoardPos.Pulled;
@@ -17,103 +15,103 @@
         currentChessboardPos = ChessBoardPos.Center;
         chessboardRendered = true;
     }, 0);
+
+    let p1Timer = "2:57";
+    let p2Timer = "2:45";
+    
+    let p1CapturedPieces = ["king", "pawn", "pawn", "rook", "bishop", "queen"];
+    let p2CapturedPieces = ["pawn", "knight", "pawn"];
 </script>
 
 {#if chessboardRendered} 
     <div id="left-aux-container" class="aux-container" transition:fade={{ delay: 400, duration: 100 }}>
-        <div id="chat-box">
-            <div id="chat-box-chats"></div>
-            <input type="text" id="chat-box-input" placeholder="type your message"/>
+        <div id="chat-box-container" class="left-aux-el">
+            <div id="chat-box-history" class="chat-box-el"></div>
+            <input type="text" id="chat-box-msg-input" class="chat-box-el" placeholder="type your message"/>
         </div>
-        <div id="match-btn-container">
-            <button class="match-btn disabled-btn">abort</button>
-            <button class="match-btn">resign</button>
-            <button class="match-btn">draw</button>
-        </div>
-    </div>
 
+        <div id="game-btn-container">
+            <button class="game-btn ft-roboto disabled">abort</button>
+            <button class="game-btn ft-roboto">resign</button>
+            <button class="game-btn ft-roboto">draw</button>
+        </div>
+    </div> 
+{/if}
+   
+<div id="chessboard-container" class="chessboard-pos-{currentChessboardPos}">
+    <Chessboard/>
+</div>
+
+{#if chessboardRendered}
     <div id="right-aux-container" class="aux-container" transition:fade={{ delay: 400, duration: 100 }}>
-        <div id="opp-container" class="player-container">
-            <button class="timer" disabled>2:55</button>
-            <div id="opp-info-container" class="player-info-container">
-                <div id="opp-name-container" class="player-info-element player-name-container">
-                    <p id="opp-name" class="player-name">opponent</p>
+        <div id="p2-status-container" class="player-status-container">
+            <button id="p2-timer" class="player-status-el player-timer disabled" disabled>{p2Timer}</button>
+            <div id="p2-status-sub-container" class="player-status-el player-status-sub-container">
+                <div id="p2-name-container" class="player-status-sub-el player-name-container">
+                    <p id="p2-name" class="player-name">guest_514</p>
                 </div>
-                <div id="opp-cp-container" class="player-info-element player-cp-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="chess-piece wcp cr-el">
-                        {@html chessPieces["bishop"].svg}
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="chess-piece wcp cr-el">
-                        {@html chessPieces["bishop"].svg}
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="chess-piece wcp cr-el">
-                        {@html chessPieces["bishop"].svg}
-                    </svg>
-                    <!-- <p class="chess-end cr-el">+1</p> -->
-                     +1
+                <div id="p2-cap-pieces-container" class="player-status-sub-el player-cap-pieces-container">
+                    {#each p2CapturedPieces.slice(0, 5) as piece}
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="captured-piece white">
+                            {@html chessPieces[piece].svg}
+                        </svg>
+                    {/each}
+                    {p2CapturedPieces.length > 5 ? `+ ${p2CapturedPieces.length - 5}`  : ""}
                 </div>
             </div>
         </div>
+    
         <div id="move-board"></div>
-        <div id="user-container" class="player-container">
-            <button class="timer disabled" disabled>2:55</button>
-            <div id="user-info-container" class="player-info-container">
-                <div id="user-cp-container" class="player-info-element player-cp-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="chess-piece bcp cr-el">
-                        {@html chessPieces["bishop"].svg}
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="chess-piece bcp cr-el">
-                        {@html chessPieces["bishop"].svg}
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="chess-piece bcp cr-el">
-                        {@html chessPieces["bishop"].svg}
-                    </svg>
-                    <!-- <p class="chess-end cr-el">+1</p> -->
-                    +1
+
+        <div id="p1-status-container" class="player-status-container">
+            <button id="p1-timer" class="player-status-el player-timer">{p1Timer}</button>
+            <div id="1-status-sub-container" class="player-status-el player-status-sub-container">
+                <div id="p1-cap-pieces-container" class="player-status-sub-el player-cap-pieces-container">
+                    {#each p1CapturedPieces.slice(0, 5) as piece}
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 40" class="captured-piece black">
+                            {@html chessPieces[piece].svg}
+                        </svg>
+                    {/each}
+                    {p1CapturedPieces.length > 5 ? `+${p1CapturedPieces.length - 5}` : ""}
                 </div>
-                <div id="user-name-container" class="player-info-element player-name-container">
-                    <p id="user-name" class="player-name">opponent</p>
+                <div id="p1-name-container" class="player-status-sub-el player-name-container">
+                    <p id="p1-name" class="player-name">dedcliff</p>
                 </div>
             </div>
         </div>
     </div>
 {/if}
 
-<div id="chessboard-container" class="chessboard-pos-{currentChessboardPos}">
-    <Chessboard/>
-</div>
-
-
 <style>
     .aux-container {
         position: fixed;
-        height: 30vw;
-        width: 20vw;
         top: 50vh;
         transform: translateY(-50%);
+        height: 30vw;
+        width: 19vw;
     }
 
     #left-aux-container {
         left: 5%;
     }
 
-    #chat-box {
-        position: relative;
-        height: 18vw;
-        width: 100%;
+    #chat-box-container {
+        position: fixed;
         top: 50%;
         transform: translateY(-50%);
+        height: 18vw;
+        width: 100%;
         border: 1px solid #d1d1d1;
     }
 
-    #chat-box-chats {
+    #chat-box-history {
         position: fixed;
         top: 0px;
         height: calc(100% - 44px);
         width: 100%;
     }
 
-    #chat-box-input {
+    #chat-box-msg-input {
         position: fixed;
         bottom: .5px;
         height: 40px;
@@ -127,44 +125,41 @@
         color: #313131;
     }
 
-    #chat-box-input::placeholder {
+    #chat-box-msg-input::placeholder {
         color: #a2a2a2;
     }
 
-    #match-btn-container {
+    #game-btn-container {
         position: fixed;
         bottom: 0px;
-        /* border: 1px solid green; */
         height: 40px;
         width: 100%;
         text-align: center;
     }
 
-    .match-btn {
+    .game-btn {
         height: 100%;
         width: 30%;
-        /* margin-right: 3.6%; */
-        font-family: "Roboto Mono", sans-serif;
         font-size: 14px;
-        border: none;
         color: #a2a2a2;
+        border: none;
         background: #efefef;
         cursor: pointer;
     }
 
-    .match-btn:not(.disabled-btn):hover {
+    .game-btn:not(.disabled):hover {
         color: #313131;
     }
 
-    .match-btn.disabled-btn {
+    .game-btn.disabled {
         color: #d1d1d1;
     }
 
-    .match-btn:nth-of-type(1) {
+    .game-btn:first-of-type {
         float: left;
     }
 
-    .match-btn:nth-of-type(3) {
+    .game-btn:last-of-type {
         float: right;
     }
 
@@ -172,124 +167,118 @@
         right: 5%;
     }
 
-    #move-board {
+    .player-status-container {
+        height: 55px;
+        width: 100%;
+        /* border: 1px solid black; */
+    }
+
+    .player-timer {
+        float: left;
+        height: 100%;
+        width: 120px;
+        font-size: 30px;
+        font-family: "Space Mono", sans-serif;
+        font-weight: bold;
+        color: #313131;
+        background: #efefef;
+        border: none;
+    }
+
+    .player-timer.disabled {
+        color: #a2a2a2;
+    }
+
+    .player-status-sub-container {
+        float: right;
+        height: 100%;
+        width: calc(100% - 122px);
+    }
+
+    .player-status-sub-el {
+        width: 100%;
+        margin: 0px;
+    }
+
+    .player-name-container {
+        height: 22px;
+    }
+
+    .player-name {
+        float: right;
+        font-family: "Space Mono", sans-serif;
+        font-size: 18px;
+        line-height: 0px;
+        margin-bottom: 0px;
+    }
+    
+    .player-cap-pieces-container {
         position: relative;
+        height: 31px;
+        color: #a2a2a2;
+        font-family: "Space Mono", sans-serif;
+        font-size: 17px;
+        display: inline-block;
+        text-align: right;
+    }
+
+    .captured-piece {
+        position: relative;
+        height: 35px;
+        margin-right: -5px;
+        stroke: #313030;
+        stroke-width: .6px;
+        vertical-align: middle;
+        display: inline-block;
+    }
+
+    .captured-piece:last-of-type {
+        margin-right: -10px;
+    }
+
+    .captured-piece.white {
+        fill: #fff;
+    }
+
+    .captured-piece.black {
+        fill: #a2a2a2;
+    }
+
+    #p1-status-container {
+        position: fixed;
+        bottom: 0px;
+    }
+    
+    #p1-name {
+        margin-top: 11px;
+    }
+
+    #p1-name-container {
+        margin-top: 2px    ;
+    }
+
+    #p2-status-container {
+        position: fixed;
+        top: 0px;
+    }
+
+    #p2-name {
+        margin-top: 7px;
+    }
+
+    #p2-cap-pieces-container {
+        margin-top: 2px;
+    }
+
+    #move-board {
+        position: fixed;
         height: 18vw;
         width: 100%;
         top: 50%;
         transform: translateY(-50%);
         background: #efefef;
     } 
-
-    .player-container {
-        position: fixed;
-        height: 55px;
-        width: 100%;
-    }
     
-    #opp-container {
-        top: 0px;
-    }
-    
-    #user-container {
-        bottom: 0px;
-    }
-
-    .timer {
-        height: 100%;
-        width: 120px;
-        background: #efefef;
-        float: left;
-        font-family: "Space Mono", sans-serif;
-        font-weight: bold;
-        font-size: 30px;
-        color: #313131;
-        border: none;
-    }
-
-    .timer.disabled {
-        color: #a2a2a2;
-    }
-
-    .player-info-container {
-        height: 100%;
-        width: 175px;
-        float: right;
-        /* margin-right: 0px; */
-    }
-
-    .player-info-element {
-        background: 1px solid blue;
-        float: right;
-    }
-
-    .player-name-container {
-        height: 35%;
-        width: 175px;
-    }
-
-    /* why does this work??!! */
-    .player-name {
-        line-height: 0px;
-        margin-top: 7px;
-        float: right;
-        font-family: "Space Mono", sans-serif;
-        font-size: 18px;
-    }
-
-    #opp-name {
-        color: #313131;
-    }
-
-    #user-name {
-        color: #a2a2a2;
-    }
-
-    .player-cp-container {
-        height: 35px;
-        font-family: "Space Mono", sans-serif;
-        font-size: 17px;
-        /* border: 1px solid black; */
-        margin-right: -2px;
-        color: #a2a2a2;
-    }
-
-    #opp-cp-container {
-        margin-top: 7px;
-    }
-
-    .chess-piece {
-        margin: 0px;
-        position: relative;
-        height: 35px;
-        vertical-align: middle;
-        display: inline-block;
-        margin-right: -30px;
-        stroke: #313030;
-        stroke-width: .6px;
-    }
-
-    .chess-piece:nth-of-type(1) {
-        z-index: 2;
-    }
-
-    .chess-piece:nth-of-type(2) {
-        z-index: 1;
-    }
-
-    .chess-piece:nth-of-type(3) {
-        z-index: 0;
-        margin-right: -10px;
-    }
-
-    .wcp {
-        fill: #fff;
-    }
-
-    .bcp {
-        fill: #a2a2a2;
-    }
-
     #chessboard-container {
         position: fixed;
         margin-left: 50%;
