6b08ff1e92232f56af4510e241a19c69ac7c0864
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-07-05 01:05:32 +0530

    client: update: types: player & color

--- client/src/lib/types/general.ts
+++ client/src/lib/types/general.ts
@@ -1,3 +1,17 @@
+export interface Player {
+    name: string,
+    isOne: boolean, // needs better name
+    isActive: boolean;
+    color: Color
+    timer: string, // needs different type
+    capturedPieces: Array<string>
+}
+
+export enum Color {
+    White = "white",
+    Black = "black"
+}
+
 export enum AuthType {
     Login,
     Register
