03e466a0dffd45169f5bcc9fc742a08a8db0ddce
shashwatah <31614030+shashwatah@users.noreply.github.com>
2024-08-05 20:30:13 +0530

    client: update: vite port; add dep: chess, ws

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: { // took this from reddit, use dotenv later
+		host: "localhost",
+		port: 4000,
+		strictPort: true
+	}
 });
